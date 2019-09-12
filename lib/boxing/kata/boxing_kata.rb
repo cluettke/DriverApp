9507af05138cf02ea6982598fa64bfca2f7ad9cf
cluettke <cluettke@users.noreply.github.com>
2019-09-11 20:46:56 -0400

    Updating Boxing_kata and DevelopmentNotes.txt
    I have updated boxing_kata.rb to take the family configuration file and produce the starter boxes and refill boxes per the instructions.

--- lib/boxing/kata/boxing_kata.rb
+++ lib/boxing/kata/boxing_kata.rb
@@ -1,4 +1,7 @@
 require "boxing/kata/version"
+require "boxing/kata/box_scheduler"
+require "boxing/kata/family"
+require 'csv'
 
 module Boxing
   module Kata
@@ -7,8 +10,14 @@ module Boxing
       unless has_input_file?
         puts "Usage: ruby ./bin/boxing-kata <spec/fixtures/family_preferences.csv"
       end
-
-      # Starting point for your code...
+      
+      cmd_line = STDIN.gets
+      cmd_line = "spec/fixtures/family_preferences.csv"
+      scheduler = BoxScheduler.new
+      starWarsFamily = Family.new
+      print starWarsFamily.load_family_preferences(cmd_line)
+      print scheduler.ship_starter_box(starWarsFamily)
+      print scheduler.ship_refill_boxes(starWarsFamily)
     end
 
     def self.has_input_file?
